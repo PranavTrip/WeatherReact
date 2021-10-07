90483e64591ef2540c92a80c10d480d219dcfc0a
LazyCoder-1506 <pranayan1506@gmail.com>
2021-10-07 09:01:24 +0530

    made site responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
-
+ 
 const App = () => {
   const [city, setCity] = useState(null);
   const [search, setSearch] = useState("Kanpur");
-
+ 
   useEffect(() => {
     const fetchAPI = async () => {
       const response =
@@ -17,49 +17,54 @@ const App = () => {
     };
     fetchAPI();
   }, [search]);
-
+ 
   return (
     <>
-      <div className="container container-fluid ">
-        <h1 className="text-center">Weather App</h1>
-        <hr />
-        <div className="card">
-          <div className="card-body">
-            <div className="input-group mb-3">
-              <input
-                type="text"
-                className="form-control"
-                aria-label="Sizing example input"
-                aria-describedby="inputGroup-sizing-default"
-                placeholder="Search Your City Here"
-                onChange={(event) => setSearch(event.target.value)}
-              />
-              <i class="fa fa-search search-btn"></i>
-            </div>
-
-            {city ? (
-              <div>
-                <h3 className="text-center m-3 city-desc"> {search} </h3>
-                <h2 className="text-center m-5 temp-desc">{city.temp} °cel</h2>
-                <div className="mt-10 text-center">
-                  <span className="m-2 text-center min-temp">
-                    | Minimum Temperature:{city.temp_min} °cel |
-                  </span>
-                  <span className="m-2 text-center max-temp">
-                    | Maximum Temperature:{city.temp_max} °cel |
-                  </span>
+      <div className="container">
+        <div className="row mx-0">
+          <div className="col-12 col-md-8 col-lg-6 offset-md-2 offset-lg-3">
+            <h1 className="text-center">Weather App</h1>
+            <hr />
+            <div className="card w-100">
+              <div className="card-body">
+                <div className="input-group mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    aria-label="Sizing example input"
+                    aria-describedby="inputGroup-sizing-default"
+                    placeholder="Search Your City Here"
+                    onChange={(event) => setSearch(event.target.value)}
+                  />
+                  <i class="fa fa-search search-btn"></i>
                 </div>
+ 
+                {city ? (
+                  <div>
+                    <h3 className="text-center my-3 city-desc"> {search} </h3>
+                    <h2 className="text-center my-5 temp-desc">{city.temp} °cel</h2>
+                    <div className="mt-10 text-center">
+                      <span className="m-2 text-center min-temp d-inline-block">
+                        | Minimum Temperature:{city.temp_min} °cel |
+                      </span>
+                      <span className="m-2 text-center max-temp d-inline-block">
+                        | Maximum Temperature:{city.temp_max} °cel |
+                      </span>
+                    </div>
+                  </div>
+                ) : (
+                  <p className="error text-center">
+                    No Data Found, Please Enter a valid city
+                  </p>
+                )}
               </div>
-            ) : (
-              <p className="error text-center">
-                No Data Found, Please Enter a valid city
-              </p>
-            )}
+            </div>
           </div>
         </div>
       </div>
     </>
   );
 };
-
+ 
 export default App;
+ 
