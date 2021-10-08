5e3ecc996dc700a67073bd50378cdb0d9f7bbe02
unknown <omkarjadhav095@gmail.com>
2021-10-08 15:09:32 +0530

    Footer is Added.

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+    return (
+    <footer>
+        <p>Made with ❤️ by <a href="https://github.com/PranavTrip">Pranav</a> & Team.</p>
+    </footer>
+    )
+}
+
+export default Footer
